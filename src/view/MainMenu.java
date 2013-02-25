bb356615db3e1ba2a107ab4809b70bc28f6785c6
Quentin <Quentin@192.168.1.10>
2013-02-25 16:42:33 +0100

    ajout menu

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -4,46 +4,78 @@
  */
 package view;
 
-import org.newdawn.slick.AppGameContainer;
+import model.Game;
 import org.newdawn.slick.GameContainer;
+
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.ComponentListener;
+import org.newdawn.slick.gui.MouseOverArea;
 /**
  *
- * @author Ara
+ * @author Quentin
  */
-public class MainMenu extends BasicGameState{
+public class MainMenu extends BasicGameState implements ComponentListener{
+
     private int stateID;
-    private Test test;
 
-    public MainMenu(int stateID){
+    private StateBasedGame game;
+    private GameContainer container;
+
+    /* les deux boutons en bas du ChoixState*/
+
+    private MouseOverArea quit;
+    private MouseOverArea play;
+    
+    public MainMenu (int stateID) {
         this.stateID=stateID;
-        test = new Test(0);
     }
-
+    
     @Override
     public int getID() {
-        return stateID;
+        return this.stateID;
     }
 
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
-        test.init(container, game);
+        this.game=game;
+        this.container=container;
+        quit = new MouseOverArea(container,new Image(getClass().getResource("ressources/quit.png").getPath()), 350, 620,this);
+        quit.setNormalColor(new Color(0.7f,0.7f,0.7f,1f));
+        quit.setMouseOverColor(new Color(0.9f,0.9f,0.9f,1f));
+
+        play = new MouseOverArea(container,new Image(getClass().getResource("ressources/newgame.png").getPath()), 350, 430, this);
+        play.setNormalColor(new Color(0.7f,0.7f,0.7f,1f));
+        play.setMouseOverColor(new Color(0.9f,0.9f,0.9f,1f));
 
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-        container.sleep(20);
-        test.render(container, game, g);
+        quit.render(container, g);
+        play.render(container, g);
+        g.drawString("Hello dans MenuState",100,100);
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-        test.update(container, game, delta);
+    //throw new UnsupportedOperationException("Not supported yet.");
     }
+    
+    @Override
+    public void componentActivated(AbstractComponent source) { //methode de l'interface ComponentListener
+        if (source == quit) {
+            this.container.exit();
+        }
+        if (source == play) {
+            game.enterState(1);
+        }
+    } 
 
 }
