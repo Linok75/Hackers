94aa9ebf86ab1bd20c921646147724f73966a209
Quentin <Quentin@192.168.1.10>
2013-03-18 16:19:01 +0100

    Ajout des attaques

---
+++ src/model/ressources/attacks/Effraction.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.ressources.attacks;
+
+import model.Game;
+import model.maps.Defence;
+import model.maps.Node;
+
+/**
+ *
+ * @author Quentin
+ */
+public class Effraction extends Attack {
+    public Effraction() {
+        super("Effraction", "...", Defence.Effraction, 15);
+    }
+
+    public void execute(Node node) {
+        Game.getInstance().getLevel().getMap().effraction(node);
+    }
+}
