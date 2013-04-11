06cab4368741bca4f3091eb9a230b189997c9c26
MyMac <MyMac@192.168.1.38>
2013-04-12 00:30:50 +0200

    idée effraction

--- src/model/ressources/attacks/DF_Hexa/DF_Hexa_Effraction.java
+++ src/model/ressources/attacks/DF_Hexa/DF_Hexa_Effraction.java
@@ -15,35 +15,20 @@ import model.ressources.attacks.DiffusionMethod;
  * @author Ara
  */
 public class DF_Hexa_Effraction extends DiffusionMethod{
-    private int radius;
 
     public DF_Hexa_Effraction(Attack attack, MapHexa map){
         super(attack, map);
-        this.radius = 1;
     }
 
     @Override
     protected ArrayList<Node> getAllNodesAround(Node node) {
         MapHexa map = (MapHexa) this.map;
         ArrayList<Node> nodes = new ArrayList<Node>();
-        Node tmpEst=node, tmpOuest=node, tmpSudEst=node, tmpSudOuest=node, tmpNordEst=node, tmpNordOuest=node;
+        addNodeIfNotNull(nodes, map.getNordEst(map.getEst(node)));
+        addNodeIfNotNull(nodes, map.getNordOuest(map.getOuest(node)));
 
-        for(int i=0;i<this.radius;i++){
-            tmpEst=map.getEst(tmpEst);
-            tmpOuest=map.getEst(tmpOuest);
-            tmpSudEst=map.getEst(tmpSudEst);
-            tmpSudOuest=map.getEst(tmpSudOuest);
-            tmpNordEst=map.getEst(tmpNordEst);
-            tmpNordOuest=map.getEst(tmpNordOuest);
-        }
-        addNodeIfNotNull(nodes, tmpEst);
-        addNodeIfNotNull(nodes, tmpOuest);
-        addNodeIfNotNull(nodes, tmpSudEst);
-        addNodeIfNotNull(nodes, tmpSudOuest);
-        addNodeIfNotNull(nodes, tmpNordEst);
-        addNodeIfNotNull(nodes, tmpNordOuest);
-
-        this.radius++;
+        addNodeIfNotNull(nodes, map.getSudEst(map.getEst(node)));
+        addNodeIfNotNull(nodes, map.getSudOuest(map.getOuest(node)));
 
         return nodes;
     }
